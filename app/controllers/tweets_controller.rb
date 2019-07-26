15b655978078cfc8e4c63cdaffa126981cc1b6ab
hidakago <hidakago@gmail.com>
2019-07-26 15:33:52 +0900

    投稿機能、投稿確認機能の追加、Gemfileからコメント行削除

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,28 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
-  
+
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(content: params[:tweet][:content])
+    if params[:back]
+      render :new
+    else
+      if @tweet.save
+        # 一覧画面へ遷移して"投稿を完了しました！"とメッセージを表示します。
+        redirect_to tweets_path, notice: "投稿を完了しました！"
+      else
+        # 投稿画面を再描画します。
+        render 'new'
+      end
+    end
+  end
+
+  def confirm
+    @tweet = Tweet.new(content: params[:tweet][:content])
+    render :new if @tweet.invalid?
   end
 end
