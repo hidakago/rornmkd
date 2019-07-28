56f39c577b3dce5609176fa2c007e796e6fb1061
hidakago <hidakago@gmail.com>
2019-07-28 16:07:54 +0900

    編集機能追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :update]
+
   def index
     @tweets = Tweet.all
   end
@@ -10,7 +12,7 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(content: params[:tweet][:content])
     if params[:back]
-      render :new
+      render 'new'
     else
       if @tweet.save
         # 一覧画面へ遷移して"投稿を完了しました！"とメッセージを表示します。
@@ -25,5 +27,25 @@ class TweetsController < ApplicationController
   def confirm
     @tweet = Tweet.new(content: params[:tweet][:content])
     render :new if @tweet.invalid?
+  end
+
+  def update
+    # 編集した内容をセット
+    @tweet.content = params[:tweet][:content]
+    if @tweet.invalid?
+      render 'edit'
+    else
+      if @tweet.update(content: params[:tweet][:content])
+        redirect_to tweets_path, notice: "投稿を編集しました！"
+      else
+        render 'edit'
+      end
+    end
+  end
+
+  private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
